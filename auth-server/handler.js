3e11b71cb6db032c06fc700a5525ad2e5600e23e
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-09 02:55:44 -0500

    Revert "Fix bug"
    This reverts commit f7d196a42de97aa34354533aa4d106394ce4dfa7.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,7 +15,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://jenny-jny.github.io/meet_app/"],
-  javascript_origins: ["https://jenny-jny.github.io", "http://localhost:8080"],
+  javascript_origins: ["https://jenny-jny.github.io", "http://localhost:3000"],
 };
 const {client_secret, client_id, redirect_uris, calendar_id} = credentials;
 
