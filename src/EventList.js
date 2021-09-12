a49f62a9198b8a4ed44961ea3f81e8ba43cd1b58
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-12 15:31:13 -0500

    Map events and pass in props from App to EventList to Event

--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import Event from './Event'
 
 class EventList extends Component{
   render(){
+    const {events} = this.props;
     return (
-      <ul className = "EventList"></ul>
+      <ul className = "EventList">
+        {events.map(event => {
+          return (
+            <li key = {event.id}>
+             <Event event = {event}/>
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
