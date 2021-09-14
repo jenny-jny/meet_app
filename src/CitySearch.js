43ea69e3565538acbc66939c353c5f95aa60b9af
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-13 22:29:50 -0500

    Add query state and assign it to input value

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
 class CitySearch extends Component{
+  state = {
+    query: ''
+  }
   render(){
     return (
       <div className = "CitySearch">
-        <input type = "text" className = "city"/>
+        <input type = "text" className = "city" value = {this.state.query}/>
         <ul className = "suggestions"></ul>
       </div>
     );
