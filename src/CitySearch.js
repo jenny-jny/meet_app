63350b1ca87508a0d56a94aa5d362553c98c4015
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-14 01:18:17 -0500

    Add suggestions state, map suggestions as list items in unordered list, and add last list item as "See all cities"

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 class CitySearch extends Component{
   state = {
-    query: ''
+    query: '',
+    suggestions: []
   }
 
   handleInputChanged = (event) => {
@@ -14,7 +15,16 @@ class CitySearch extends Component{
     return (
       <div className = "CitySearch">
         <input type = "text" className = "city" value = {this.state.query} onChange = {this.handleInputChanged}/>
-        <ul className = "suggestions"></ul>
+        <ul className = "suggestions">
+          {this.state.suggestions.map((suggestion) => {
+            return (
+              <li key = {suggestion}>{suggestion}</li>
+            );
+          })}
+          <li key = "all">
+            <b>See all cities</b>
+          </li>
+        </ul>
       </div>
     );
   }
