125cc9705c9ae734590ea42040de04bf36b5dac2
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-13 22:55:46 -0500

    Write test to change state when text input changes

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -16,4 +16,11 @@ describe('<CitySearch/> component', () => {
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
+  test('change state when text input changes', () => {
+    const CitySearchWrapper = shallow(<CitySearch/>);
+    CitySearchWrapper.setState({query: 'Munich'});
+    const eventObject = {target: {value: 'Berlin'}};
+    CitySearchWrapper.find('.city').simulate('change', eventObject);
+    expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  });
 });
