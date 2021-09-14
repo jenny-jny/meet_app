532332ce8137d8ec51f2a763825693dc48f22ffd
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-13 22:59:30 -0500

    Add handleInputChanged method and add onChange prop to input text box

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,10 +4,16 @@ class CitySearch extends Component{
   state = {
     query: ''
   }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({query: value});
+  }
+
   render(){
     return (
       <div className = "CitySearch">
-        <input type = "text" className = "city" value = {this.state.query}/>
+        <input type = "text" className = "city" value = {this.state.query} onChange = {this.handleInputChanged}/>
         <ul className = "suggestions"></ul>
       </div>
     );
