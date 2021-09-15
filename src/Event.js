484f0693dc11e166ed052bbfddf657c3ace4f4ba
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-15 08:14:57 -0500

    Initial commit

--- src/Event.js
+++ src/Event.js
@@ -1,24 +1,49 @@
 import React, {Component} from 'react';
 
 class Event extends Component{
+  state = {
+    details: false
+  }
+
+  handleShowDetailsClicked(){
+    this.setState({details: true});
+  }
+
+  handleHideDetailsClicked(){
+    this.setState({details: false});
+  }
+
   render(){
+    const {event} = this.props;
     return (
-      <div>
-        <div className = "title">
-          <h1 className = "summary"></h1>
+      <>
+        <div className = "collapsed">
+          <div className = "title">
+            <h1 className = "summary">{event.summary}</h1>
+          </div>
+          <div className = "line1">
+            <span className = "startTime">{event.start.dateTime}</span>
+            <span className = "startTimeZone">{event.start.timeZone}</span>
+          </div>
+          <div className = "line2">
+            <span>@</span>
+            <span className = "summary">{event.summary}</span>
+            <span>|</span>
+            <span className = "location">{event.location}</span>
+          </div>
+          {this.state.details === false &&
+            <button className = "showDetails" onClick = {() => this.handleShowDetailsClicked()}>show details</button>
+          } 
         </div>
-        <div className = "line-1">
-          <span className = "startTime"></span>
-          <span className = "startTimeZone"></span>
-        </div>
-        <div className = "line-2">
-          <span>@</span>
-          <span className = "summary"></span>
-          <span>|</span>
-          <span className = "location"></span>
-        </div>
-        <button className = "showDetailsButton"/>
-      </div>
+        {this.state.details === true && 
+          <div className = "expanded">
+            <h2 className = "aboutEvent">About event: </h2>
+            <link className = "googleCalendar" href = {event.htmlLink}>See details on Google Calendar</link>
+            <p className = "description">{event.description}</p>
+            <button className = "hideDetails" onClick = {() => this.handleHideDetailsClicked()}>hide details</button>
+          </div>
+        }
+      </>
     );
   }
 }
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -7,7 +7,7 @@ describe('<Event/> component', () => {
   let EventWrapper;
   
   beforeAll(() => {
-    EventWrapper = shallow(<Event/>);
+    EventWrapper = shallow(<Event event = {mockData[0]}/>);
   });
 
   test('render event summary', () => {
@@ -26,7 +26,58 @@ describe('<Event/> component', () => {
     expect(EventWrapper.find('.location')).toHaveLength(1);
   });
 
+  test('state when never clicked on show details button is false', () => {
+    expect(EventWrapper.state('details')).toBe(false); 
+  });
+
   test('render show details button', () => {
-    expect(EventWrapper.find('.showDetailsButton')).toHaveLength(1);
+    EventWrapper.setState({details: false});
+    expect(EventWrapper.find('.showDetails')).toHaveLength(1);
+  });
+
+  test('render "About event" sub-heading', () => {
+    EventWrapper.setState({details: true});
+    expect(EventWrapper.find('.aboutEvent')).toHaveLength(1);
+  });
+
+  test('render google calendar link', () => {
+    EventWrapper.setState({details: true});
+    expect(EventWrapper.find('.googleCalendar')).toHaveLength(1);
+  });
+
+  test('render event description', () => {
+    EventWrapper.setState({details: true});
+    expect(EventWrapper.find('.description')).toHaveLength(1);
+  });
+
+  test('render hide details button', () => {
+    EventWrapper.setState({details: true});
+    expect(EventWrapper.find('.hideDetails')).toHaveLength(1);
+  });
+
+  test('change state when click on show details button', () => {
+    EventWrapper.setState({details: false});
+    EventWrapper.find('.showDetails').simulate('click');
+    expect(EventWrapper.state('details')).toBe(true); 
+  });
+
+  test('change state when click on hide details button', () => {
+    EventWrapper.setState({details: true});
+    EventWrapper.find('.hideDetails').simulate('click');
+    expect(EventWrapper.state('details')).toBe(false); 
+  });
+
+  test('clicking on show details button should expand extra details', () => {
+    EventWrapper.setState({details: false});
+    EventWrapper.find('.showDetails').simulate('click'); 
+    expect(EventWrapper.find('.showDetails')).toHaveLength(0);
+    expect(EventWrapper.find('.expanded')).toHaveLength(1);
+  });
+
+  test('clicking on hide details button should collapse extra details', () => {
+    EventWrapper.setState({details: true});
+    EventWrapper.find('.hideDetails').simulate('click'); 
+    expect(EventWrapper.find('.showDetails')).toHaveLength(1);
+    expect(EventWrapper.find('.expanded')).toHaveLength(0);
   });
 });
