ea04dd655e173a8832c19387d6a44f8d551dff15
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-15 06:24:01 -0500

    Initial commit

--- src/Event.js
+++ src/Event.js
@@ -3,7 +3,22 @@ import React, {Component} from 'react';
 class Event extends Component{
   render(){
     return (
-      <div></div>
+      <div>
+        <div className = "title">
+          <h1 className = "summary"></h1>
+        </div>
+        <div className = "line-1">
+          <span className = "startTime"></span>
+          <span className = "startTimeZone"></span>
+        </div>
+        <div className = "line-2">
+          <span>@</span>
+          <span className = "summary"></span>
+          <span>|</span>
+          <span className = "location"></span>
+        </div>
+        <button className = "showDetailsButton"/>
+      </div>
     );
   }
 }
