18feba195b601370d2f95bd724aeb1b17f1ce3df
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-15 08:30:20 -0500

    Initial commit

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class NumberOfEvents extends Component{
+  render(){
+    return (
+      <div className = "NumberOfEvents">
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
