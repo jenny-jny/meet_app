75d764db7c3d2865f7cc832a1aae6d91eff91708
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-16 16:58:23 -0500

    Change to class component, add events state, and pass it as events prop to EventList component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
+class App extends Component {
+  state = {
+    events: []
+  }
 
-function App() {
-  return (
-    <div className = "App">
-      <EventList/>
-      <CitySearch/>
-      <NumberOfEvents/>
-    </div>
-  );
+  render(){
+    return (
+      <div className = "App">
+        <EventList events = {this.state.events}/>
+        <CitySearch/>
+        <NumberOfEvents/>
+      </div>
+    );
+  }
 }
 
 export default App;
