fb1108c0d6f6757e350442067dccf7854ae29ca2
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-15 22:59:52 -0500

    Initial commit

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
 
 class NumberOfEvents extends Component{
+  state = {
+    query: '32',
+    message: ''
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if(isNaN(value) || value < 1){
+      this.setState({message: 'Please enter a valid number'});
+    }else{
+      this.setState({query: value});
+    }
+  }
+
   render(){
     return (
-      <div className = "NumberOfEvents">
+      <div className = "NumberOfEvents" >
+        <label className = "numberOfEventsLabel">Number of events: </label>
+        <span>
+          <input type = "text" className = "numberOfEvents" value = {this.state.query} onChange = {this.handleInputChanged}/>
+        </span> 
+        <span>
+          <p className = "errorMessage">{this.state.message}</p>
+        </span>
       </div>
     );
   }
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,84 @@
+//Create tests for ensuring the textbox element is rendered correctly and for changing the event on the textbox.
+import React from 'react';
+import {shallow} from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+import {mockData} from '../mock-data';
+
+describe('<NumberOfEvents/> component', () => {
+  let NumberOfEventsWrapper;
+  
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+  });
+
+  test('state when never entered number of events to show is 32', () => {
+    expect(NumberOfEventsWrapper.state('query')).toBe('32'); 
+  });
+
+  test('state when never entered number of events to show is empty string', () => {
+    expect(NumberOfEventsWrapper.state('message')).toBe(''); 
+  });
+
+  test('render number of events label', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEventsLabel')).toHaveLength(1);
+  });
+
+  test('render number of events textbox', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+
+  test('render error message', () => {
+    expect(NumberOfEventsWrapper.find('.errorMessage')).toHaveLength(1);
+  });
+
+  test('render number of events input correctly', () => {
+    const query = NumberOfEventsWrapper.state('query');
+    expect(NumberOfEventsWrapper.find('.numberOfEvents').prop('value')).toBe(query);
+  });
+
+  test('change query state when enter a valid number of events', () => {
+    NumberOfEventsWrapper.setState({query: '2'});
+    const eventObject = {target: {value: '3'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('query')).toBe('3');
+  });
+
+  test('render error message correctly when enter an invalid number of events => a', () => {
+    const eventObject = {target: {value: 'a'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.find('.errorMessage').text()).toEqual('Please enter a valid number');
+  });
+
+  test('render error message correctly when enter an invalid number of events => -1', () => {
+    const eventObject = {target: {value: '-1'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.find('.errorMessage').text()).toEqual('Please enter a valid number');
+  });
+
+  test('render error message correctly when enter an invalid number of events => 0', () => {
+    const eventObject = {target: {value: '0'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.find('.errorMessage').text()).toEqual('Please enter a valid number');
+  });
+
+  test('change error message state when enter an invalid number of events => a', () => {
+    NumberOfEventsWrapper.setState({message: ''});
+    const eventObject = {target: {value: 'a'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('message')).toBe('Please enter a valid number');
+  });
+
+  test('change error message state when enter an invalid number of events => -1', () => {
+    NumberOfEventsWrapper.setState({message: ''});
+    const eventObject = {target: {value: '-1'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('message')).toBe('Please enter a valid number');
+  });
+
+  test('change error message state when enter an invalid number of events => 0', () => {
+    NumberOfEventsWrapper.setState({message: ''});
+    const eventObject = {target: {value: '0'}};
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('message')).toBe('Please enter a valid number');
+  });
+});
