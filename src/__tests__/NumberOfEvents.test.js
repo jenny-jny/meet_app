fdb3a77851baa4b8137d431625a22d38bdc2d020
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-16 16:31:12 -0500

    Delete import mockData

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
-import {mockData} from '../mock-data';
 
 describe('<NumberOfEvents/> component', () => {
   let NumberOfEventsWrapper;
