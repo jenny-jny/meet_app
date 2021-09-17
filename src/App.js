90d0166adb2edb78045d7a4af4fc22a9e444b9d2
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-16 23:51:13 -0500

    Update updateEvents function

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ class App extends Component {
 
   updateEvents = location => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => 
-        event.location === location
-      );
-      this.setState({events: locationEvents});
+      const locationEvents = (location === 'all')?
+        events:
+        events.filter((event) => 
+          event.location === location
+        );
+        this.setState({events: locationEvents});
     }); 
   };
 
