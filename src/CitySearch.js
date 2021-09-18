535e95827a51b81f7072a4cae0ab6fed9e1ba9c7
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-18 05:18:45 -0500

    Add placeholder attribute to textbox input

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -23,7 +23,7 @@ class CitySearch extends Component{
   render(){
     return (
       <div className = "CitySearch">
-        <input type = "text" className = "city" value = {this.state.query} onChange = {this.handleInputChanged} onFocus = {() => {this.setState({showSuggestions: true})}}/>
+        <input type = "text" className = "city" placeholder = "Search for a city" value = {this.state.query} onChange = {this.handleInputChanged} onFocus = {() => {this.setState({showSuggestions: true})}}/>
         <ul className = "suggestions" style = {this.state.showSuggestions ? {} : {display: 'none'}}>
           {this.state.suggestions.map((suggestion) => {
             return (
