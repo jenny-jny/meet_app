f36830a49f76e11f257a9d6d90db91f31d09b0a7
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-17 20:32:19 -0500

    Update handleItemClicked method to set showSuggestions to false and add style to suggestions list

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -16,7 +16,7 @@ class CitySearch extends Component{
   }
 
   handleItemClicked = (suggestion) => {
-    this.setState({query: suggestion});
+    this.setState({query: suggestion, showSuggestions: false});
     this.props.updateEvents(suggestion);
   }
  
@@ -24,7 +24,7 @@ class CitySearch extends Component{
     return (
       <div className = "CitySearch">
         <input type = "text" className = "city" value = {this.state.query} onChange = {this.handleInputChanged} onFocus = {() => {this.setState({showSuggestions: true})}}/>
-        <ul className = "suggestions">
+        <ul className = "suggestions" style = {this.state.showSuggestions ? {} : {display: 'none'}}>
           {this.state.suggestions.map((suggestion) => {
             return (
               <li key = {suggestion} onClick = {() => {this.handleItemClicked(suggestion)}}>{suggestion}</li>
