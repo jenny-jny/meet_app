4b4694fff04a8ca6f4ecaa820ab4d9f2df05cc00
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-17 20:49:00 -0500

    Import axios and add getAccessToken async function

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {mockData} from './mock-data'
 
 // This function takes an events array, then uses map to create a new array with only locations. It will also remove all duplicates by creating another new array using the spread operator and spreading a Set. The Set will remove all duplicates from the array.
@@ -6,6 +7,10 @@ export const extractLocations = (events) => {
   var locations = [...new Set(extractLocations)];
   return locations;
 };
+
+export const getAccessToken = async() => {
+
+}
 
 export const getEvents = async() => {
   return mockData;
