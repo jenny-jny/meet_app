fb52e0e57251e89fe8da1d9608ac0a3b2ddcb366
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-27 17:52:36 -0500

    Fix bug

--- src/api.js
+++ src/api.js
@@ -57,7 +57,7 @@ export const getEvents = async() => {
   if(!navigator.onLine){
     const {events} = localStorage.getItem('lastEvents');
     NProgress.done();
-    return {events: JSON.parse(events), locations: extractLocations(events)};
+    return {events: JSON.parse(events)};
   }
 
   const token = await getAccessToken();
