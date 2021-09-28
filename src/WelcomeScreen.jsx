364fe8d2eb58f5d68c6c3f9f699d2b02ad72da86
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-27 19:18:29 -0500

    Fix bug

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -7,9 +7,7 @@ function WelcomeScreen(props) {
       <div className="WelcomeScreen">
         <h1>Welcome to the Meet app</h1>
         <h4>
-          Log in to see upcoming events around the world for
-          full-stack
-          developers
+          Log in to see upcoming events around the world for full-stack developers
         </h4>
         <div className="button_cont" align="center">
           <div class="google-btn">
@@ -21,7 +19,7 @@ function WelcomeScreen(props) {
             </button>
           </div>
         </div>
-        <a href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html" rel="nofollow noopener">Privacy policy</a>
+        <a href="https://jenny-jny.github.io/meet/privacy.html" rel="nofollow noopener">Privacy policy</a>
       </div> 
     )
     : null 
