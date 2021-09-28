0686f771ead124fcf5f112a74b306b6750a528c1
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-27 20:33:54 -0500

    Fix bug

--- src/api.js
+++ src/api.js
@@ -32,7 +32,7 @@ export const getAccessToken = async () => {
 export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  ).then((res) => res.json()).catch((error) => error.json());
+  ).then((res) => res.json()).catch((error) => error);
   return result;
 };
 
