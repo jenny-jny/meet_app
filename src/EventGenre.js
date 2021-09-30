4c64d2faf674a62db068dde3e8c13442fcf84544
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-29 22:24:36 -0500

    Initial commit

---
+++ src/EventGenre.js
@@ -0,0 +1,28 @@
+import react, {useEffect, useState} from 'react';
+import {PieChart, Pie, ResponsiveContainer} from 'recharts';
+
+const EventGenre = ({events}) => {
+  useEffect(() => {setData(getData())}, [events]);
+
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+  const [data, setData] = useState([]);
+  
+  function getData(){
+    const data = genres.map(genre => {
+      const value = events.summary.filter(sum => sum.split(' ').includes(genre)).length;
+      return {name: genre, value};
+    });
+    return data;
+  }
+
+  return (
+    <ResponsiveContainer height = {400}>
+      <PieChart width = {400} height = {400}>
+        <Pie data = {getData()} cx = {200} cy = {200} labelLine = {false} label={({name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`} outerRadius = {80} fill = "#8884d8" dataKey = "value">
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenre;
