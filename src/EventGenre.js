95030a39d721fc68073a625236763b05c94ad26e
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-29 22:56:19 -0500

    Fix useEffect and getData methods bugs

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,16 +2,14 @@ import react, {useEffect, useState} from 'react';
 import {PieChart, Pie, ResponsiveContainer} from 'recharts';
 
 const EventGenre = ({events}) => {
-  useEffect(() => {setData(getData())}, [events]);
+  useEffect(() => {setData(() => getData())}, [events]);
 
   const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
   const [data, setData] = useState([]);
-  
+
   function getData(){
     const data = genres.map(genre => {
-      const value = events.map(event => {
-        return event.summary.filter(sum => sum.split(' ').includes(genre)).length;
-      });
+      const value = events.filter(event => event.summary.split(' ').includes(genre)).length;
       return {name: genre, value};
     });
     return data;
@@ -20,7 +18,7 @@ const EventGenre = ({events}) => {
   return (
     <ResponsiveContainer height = {400}>
       <PieChart width = {400} height = {400}>
-        <Pie data = {getData()} cx = {200} cy = {200} labelLine = {false} label={({name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`} outerRadius = {80} fill = "#8884d8" dataKey = "value">
+        <Pie data = {getData()} cx = {200} cy = {200} labelLine = {false} label = {({name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`} outerRadius = {80} fill = "#8884d8" dataKey = "value">
         </Pie>
       </PieChart>
     </ResponsiveContainer>
