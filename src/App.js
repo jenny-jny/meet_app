8e5e5d5828470edec67168854dd9affb784ebebc
jenny-jny <jenny.yu.0414@gmail.com>
2021-09-29 20:58:24 -0500

    Import responsive container component and wrap scatter chart in it

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NumberOfEvents from './NumberOfEvents';
 import {WarningAlert} from './Alert';
 import EventList from './EventList';
 import {extractLocations, getAccessToken, checkToken, getEvents} from './api';
-import {CartesianGrid, Scatter, ScatterChart, Tooltip, XAxis, YAxis} from 'recharts';
+import {ResponsiveContainer, CartesianGrid, Scatter, ScatterChart, Tooltip, XAxis, YAxis} from 'recharts';
 
 class App extends Component {
   state = {
@@ -93,13 +93,15 @@ class App extends Component {
         <NumberOfEvents updateNumberOfEvents = {(newCount) => this.updateNumberOfEvents(newCount)}/>
         <WarningAlert text = {infoText}/>
         <h4>Events in each city</h4>
-        <ScatterChart width = {800} height = {400} margin = {{top: 20, right: 20, left: 20, bottom: 20}}>
-          <CartesianGrid/>
-          <XAxis type = "category" dataKey = "city" name = "city"/>
-          <YAxis type = "number" dataKey = "number" name = "number of events" allowDecimals = {false}/>
-          <Tooltip cursor = {{strokeDasharray: '3 3'}}/>
-          <Scatter data = {this.getData()} fill = "#8884d8"/>
-        </ScatterChart>
+        <ResponsiveContainer height = {400}>
+          <ScatterChart margin = {{top: 20, right: 20, left: 20, bottom: 20}}>
+            <CartesianGrid/>
+            <XAxis type = "category" dataKey = "city" name = "city"/>
+            <YAxis type = "number" dataKey = "number" name = "number of events" allowDecimals = {false}/>
+            <Tooltip cursor = {{strokeDasharray: '3 3'}}/>
+            <Scatter data = {this.getData()} fill = "#8884d8"/>
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events = {events}/>
         <WelcomeScreen showWelcomeScreen = {showWelcomeScreen} getAccessToken = {() => getAccessToken()}/>
       </div>
